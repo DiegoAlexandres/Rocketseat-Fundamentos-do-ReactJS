d80fc7c2be7f39e760f830f3592d392f916230b5
DiegoAlexandres <diegoalexandre.me@gmail.com>
2024-02-03 00:31:12 -0300

    Componente Sidebar finalizado

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,19 @@
 import styles from './Sidebar.module.css'
-
-import logoDaxCode from '../assets/logo-dax-code.svg'
+import { PencilLine } from 'phosphor-react'
 
 export function Sidebar(){
     return (
         <aside className={styles.sidebar}>
-            <img src={logoDaxCode} />
+            <img className={styles.cover} src='https://images.unsplash.com/photo-1593720213428-28a5b9e94613?q=60&w=800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/DiegoAlexandres.png"/>
                 <strong>Diego Alexandre</strong>
                 <span>Web Developer</span>
             </div>
             <footer>
                 <a href="#">
+                    <PencilLine size={16}/>
                     Editar seu perfil
                 </a>
             </footer>
