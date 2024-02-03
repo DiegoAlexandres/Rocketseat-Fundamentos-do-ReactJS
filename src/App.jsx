1edd23a5be9fe585e39282106485bd47dc372cbb
DiegoAlexandres <diegoalexandre.me@gmail.com>
2024-02-02 23:07:15 -0300

    Adicionando estilos globais na aplicação + Primeiros componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { Post } from './Post.jsx';
+import { Header } from './components/Header.jsx';
+
+import './global.css';
+
 export function App() {
+  return (
+    <div>
+      <Header />
 
-  return <h1>Hello World</h1>
-
+      <Post
+        author="Diego Alexandre"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi dolorem sunt eligendi rem repellat iure esse, delectus distinctio velit ea ratione eius dolore inventore officia ipsam ipsum suscipit nemo quasi!"
+      />
+      <Post
+        author="João"
+        content="Meu primeiro post"
+      />
+    </div>
+  )
 }
+
