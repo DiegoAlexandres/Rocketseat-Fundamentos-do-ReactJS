c2205b015f131ba6bb5f146a1972e6770bf45b48
DiegoAlexandres <diegoalexandre.me@gmail.com>
2024-02-10 11:20:20 -0300

    Componente Avatar adicionado na aplicação

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,7 +7,7 @@ export function Post(){
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/DiegoAlexandres.png"/>  
+                    <Avatar src="https://github.com/diegoalexandres.png"/> 
                     <div className={styles.authorInfo}>
                     <strong>Diego Alexandre</strong>
                     <span>Dev Full Stack</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
-import styles from './Sidebar.module.css'
-import { PencilLine } from 'phosphor-react'
+import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
+import { PencilLine } from 'phosphor-react';
 
 export function Sidebar(){
     return (
@@ -7,7 +8,7 @@ export function Sidebar(){
             <img className={styles.cover} src='https://images.unsplash.com/photo-1593720213428-28a5b9e94613?q=60&w=800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/DiegoAlexandres.png"/>
+                <Avatar src="https://github.com/diegoalexandres.png"/>
                 <strong>Diego Alexandre</strong>
                 <span>Web Developer</span>
             </div>
